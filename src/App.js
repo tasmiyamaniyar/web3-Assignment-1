4849c24c0e1e34b9c07dbddd8f1531832e5ddff7
Tasmiya <tasmiyamaniyar123@gmail.com>
2022-09-15 13:01:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        <h1> Web page</h1>
+        A web page (or webpage) is a hypertext document on the World Wide Web.
+        Web pages are delivered by a web server to the user and displayed in a web browser.
+        A website consists of many web pages linked together under a common domain name.
+      </p>
+      <a href="/" className="header-logo">https://en.wikipedia.org/wiki/Web_page</a>
+    </div>
+  );
+}
+
+export default App;
